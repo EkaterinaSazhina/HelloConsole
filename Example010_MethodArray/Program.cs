11b8d32094a7b077faa019d64d06789807afcaa9
Ekaterina <teos2910@mail.ru>
2023-10-08 13:56:27 +0500

    example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 11, 12, 31, 14, 51, 16, 71, 18 };
+int n = array.Length;
+int find = 31;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+        index++;
+}
