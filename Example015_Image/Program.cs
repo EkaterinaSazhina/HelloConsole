b701813bac39d74eea916f0a39fb6c4e37d81ece
Ekaterina <teos2910@mail.ru>
2023-10-22 01:05:59 +0500

    task38

---
+++ Example015_Image/Program.cs
@@ -0,0 +1,14 @@
+﻿int[,] pic = new int[,];
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++) ;
+        {
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+PrintImage(pic);
