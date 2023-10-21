d71545d3a8a7dc99381c4fe1a78e1f508383bf0b
Ekaterina <teos2910@mail.ru>
2023-10-22 02:22:45 +0500

    task38

--- Example015_Image/Program.cs
+++ Example015_Image/Program.cs
@@ -1,4 +1,4 @@
-﻿int[,] pic = new int[,];
+﻿int[,] pic = new int[20,23];
 void PrintImage(int[,] image)
 {
     for (int i = 0; i < image.GetLength(0); i++)
@@ -11,4 +11,17 @@ void PrintImage(int[,] image)
         Console.WriteLine();
     }
 }
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+PrintImage(pic);
+FillImage(13, 13);
 PrintImage(pic);
